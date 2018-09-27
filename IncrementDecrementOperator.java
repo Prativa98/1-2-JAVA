d9e4ff698ef68fa4833111f5de1b2d22f13b1b97
Prativa98 <38049873+Prativa98@users.noreply.github.com>
2018-09-28 02:37:35 +0600

    Add files via upload

---
+++ IncrementDecrementOperator.java
@@ -0,0 +1,16 @@
+package pkg15.incrementdecrementoperator;
+
+public class IncrementDecrementOperator {
+
+    public static void main(String[] args) {
+        int a=5,b;
+        b=a++;
+        System.out.println("the value of b is:"+b);
+        System.out.println("the value of a is:"+a);
+        /*b=++a;
+        System.out.println("the value of b is:"+b);
+        System.out.println("the value of a is:"+a);
+        */
+    }
+    
+}
---
+++ IncrementDecrementOperator2.java
@@ -0,0 +1,15 @@
+
+package pkg16.incrementdecrementoperator2;
+
+public class IncrementDecrementOperator2 {
+
+    public static void main(String[] args) {
+       int i=10,j;
+       j=++i;
+       System.out.println("the value of i is:"+i);//i=11
+       System.out.println("the value of i is:"+j);//j=11
+       
+               
+    }
+    
+}
